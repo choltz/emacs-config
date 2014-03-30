0bce4fba4053274f0c2621337c17c0cca2824a9e
Chris Holtz <choltz@gmail.com>
2014-03-30 13:37:02 -0400

    first commit

---
+++ init.el
@@ -0,0 +1,30 @@
+;; init.el - chiefly reserved for installing/loading/requiring packages
+(require 'cl)
+
+;;
+;; PACKAGE MANAGEMENT
+;;
+(require 'package)
+; (add-to-list 'load-path "~/.emacs.d/")
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("melpa"     . "http://melpa.milkbox.net/packages/"))
+
+(package-initialize)
+(setq package-enable-at-startup nil)
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+(defvar my-packages '(key-chord
+                      smex)
+  "A list of packages to ensure are installed at launch.")
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
+
+;;
+;; LOAD CONFIGURATION FILES
+;;
+(add-to-list 'load-path "~/source/emacs-config/")
+(load "config")
+(load "keybindings")
