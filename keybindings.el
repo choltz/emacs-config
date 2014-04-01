c0b0bb821281d10135beade7cf99d20fe22408f9
Chris Holtz <choltz@gmail.com>
2014-03-31 20:39:36 -0400

    Started adding keymaps

--- keybindings.el
+++ keybindings.el
@@ -1,4 +1,13 @@
 
+(global-set-key [C-tab] 'mode-line-other-buffer)
+(global-set-key [insert]         'nil)
+(global-set-key [f4]             'bs-show)
+(global-set-key (kbd "C-`")      'bs-show)
+
+(global-set-key (kbd "C-c g")    'magit-status)
+(global-set-key (kbd "C-c C-g")  'magit-status)
+
+
 
 ;; (global-set-key "\M-[1;2C"    'right-char)      ; Ctrl+right   => forward word
 ;; (global-set-key "\M-[1;2D"    'backward-word)     ; Ctrl+left    => backward word
