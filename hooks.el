e6e742d306393b587a824f9936acd46539147885
Chris Holtz <choltz@gmail.com>
2014-04-16 14:32:37 -0400

    Added more mode hooks

--- hooks.el
+++ hooks.el
@@ -1,10 +1,25 @@
+;; general
 (add-hook 'before-save-hook 'whitespace-cleanup)
+
+;; css
+(add-hook 'css-mode-hook
+          '(lambda ()
+             (rainbow-mode)))
 
 ;; jabber
 (add-hook 'jabber-chat-mode-hook (lambda ()
                                    (visual-line-mode)
                                    (turn-on-flyspell)
                                    (text-scale-set -2)))
+;; js2 mode
+(add-hook 'js2-mode-hook
+          '(lambda ()
+             (fci-mode)))
+
+;; markdown
+(add-hook 'markdown-mode-hook
+          (lambda ()
+            (visual-line-mode)))
 
 ;; ruby
 (add-hook 'ruby-mode-hook
@@ -27,3 +42,14 @@
                            (define-key sql-mode-map  [(ctrl f4)] 'sql-send-region)
                            (sql-set-product 'mysql)
                            (sql-highlight-mysql-keywords)))
+
+;; text mode
+(add-hook 'text-mode-hook (lambda ()
+                            (turn-on-flyspell)
+                            (visual-line-mode)))
+
+;; web mode
+(add-hook 'web-mode-hook
+          '(lambda ()
+             (rainbow-mode)
+             (auto-complete-mode)))
