f7c2d95e3f93d23fde5242a481d9cd1136f8d465
Chris Holtz <choltz@gmail.com>
2014-10-09 10:05:14 -0400

    Added dockerfile mode

--- init.el
+++ init.el
@@ -22,6 +22,7 @@
                        calfw
                        color-theme
                        dired+
+                       dockerfile-mode
                        expand-region
                        fill-column-indicator
                        fiplr
