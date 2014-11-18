c37369a917f8e4aba185ec16f026a42f7faec345
Chris Holtz <choltz@gmail.com>
2014-11-18 10:38:52 -0500

    mu4e configuration

--- mu4e-settings.el
+++ mu4e-settings.el
@@ -4,7 +4,7 @@
 (setq mu4e-drafts-folder "/Drafts")
 (setq mu4e-sent-folder   "/Sent")
 (setq mu4e-trash-folder  "/Trash")
-(setq mu4e-refile-folder "~/docs/i4cp/mail_archive")
+(setq mu4e-refile-folder "/mail_archive")
 (setq mu4e-attachment-dir "~/downloads")
 
 ;; setup some handy shortcuts
@@ -12,26 +12,33 @@
 ;; then, when you want archive some messages, move them to
 ;; the 'All Mail' folder by pressing ``ma''.
 (setq mu4e-maildir-shortcuts
-      '( ("/INBOX"               . ?i)
-         ("/[Gmail].Sent Mail"   . ?s)
-         ("/[Gmail].Trash"       . ?t)
-         ("/[Gmail].All Mail"    . ?A)
-         ("/arcana"              . ?r)
-         ("/Company"             . ?c)
-         ("/assembla"            . ?a)))
+      '( ("/INBOX"        . ?i)
+         ("/Sent"         . ?s)
+         ("/mail_archive" . ?a)
+         ("/Trash"        . ?t)))
 
 ;; General settings
 (setq mu4e-sent-messages-behavior 'delete) ; don't save message to Sent Messages, Gmail/IMAP takes care of this
 (setq message-kill-buffer-on-exit t) ; don't keep message buffers around
 
 ;; Retrieve mail settings
+;; (setq mu4e-get-mail-command "offlineimap")
+;; (setq mu4e-update-interval 60)
 (setq mu4e-get-mail-command nil) ; pull email via crontab script
-(setq mu4e-get-mail-command "offlineimap")
-(setq mu4e-update-interval 60)
+
+
+;; Hack to get address completion to work properly
+;; Note - if you 'M-x mu4e' first, then you don't need this because everything
+;; initializes properly
+(setq mu4e-contacts-func 'mu4e~fill-contacts)
+(mu4e~proc-contacts nil nil)
 
 ;; Message display settings
-(setq mu4e-view-prefer-html nil)
-(setq mu4e-html2text-command "w3m -dump -T text/html")
+(setq mu4e-view-prefer-html t)
+;;(setq mu4e-html2text-command "w3m -dump -T text/html")
+(require 'mu4e-contrib)
+(setq mu4e-html2text-command 'mu4e-shr2text)
+
 (setq mu4e-view-show-images t)
 (when (fboundp 'imagemagick-register-types) (imagemagick-register-types))
 (add-to-list 'mu4e-view-actions '("View in browser" . mu4e-action-view-in-browser) t)
