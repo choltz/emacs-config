4ffa3ea7af4ed1638f6a7163d1dbad311b3fa49b
Chris Holtz <choltz@gmail.com>
2016-11-18 10:02:55 -0500

    CDH - 2016-11-18 - emacs configuration updates

--- config.el
+++ config.el
@@ -21,6 +21,7 @@
 (popwin-mode 1)
 (global-auto-complete-mode t) ; only dev modes?
 (scroll-bar-mode 0)
+(save-place-mode)
 
 (setq-default truncate-lines t)
 (fset 'yes-or-no-p 'y-or-n-p) ; stop forcing me to spell out "yes"
@@ -28,10 +29,6 @@
 (setq bs-must-always-show-regexp "\\*scratch\\*\\|*magit: scripts*\\|\\\*^.*")
 (setq vc-follow-symlinks t)
 (setq create-lockfiles nil)
-
-(require 'saveplace)
-(setq-default save-place t)
-;; (save-place-mode) ; for emacs 25.1
 
 ;; Silence warning when processes are active
 (defadvice save-buffers-kill-emacs (around no-query-kill-emacs activate)
@@ -77,24 +74,30 @@
 
 ;; ag
 (setq ag-reuse-window t)
+
 ;; autocomplete
 (setq ac-comphist-file  "~/src/emacs-config/ac-comphist.dat")
 (setq ac-use-quick-help t)
 (setq ac-fuzzy-complete t)
 (add-to-list 'ac-modes 'web-mode)
+
 ;; bm
 (setq bm-highlight-style 'bm-highlight-only-line)
+
 ;; cua
 (setq cua-keep-region-after-copy t)
+
 ;; css mode
 (setq css-indent-offset 2)
+
 ;; dired
 (setq dired-isearch-filenames t)
 (toggle-diredp-find-file-reuse-dir 1)
 (setq dired-listing-switches "-alh --group-directories-first")
+
 ;; ediff - don't spawn a new frame
 (setq ediff-window-setup-function 'ediff-setup-windows-plain)
-; (setq ediff-split-window-function 'split-window-horizontally)
+;; (setq ediff-split-window-function 'split-window-horizontally)
 (setq ediff-split-window-function (if (> (frame-width) 150)
                                       'split-window-horizontally
                                     'split-window-vertically))
@@ -121,22 +124,28 @@
 (eval-after-load 'flycheck
   '(flycheck-credo-setup))
    (add-hook 'elixir-mode-hook 'flycheck-mode)
+
 ;; fill column mode
 (setq fci-rule-column 100)
 (setq fci-rule-color "#444444")
 (setq fci-rule-use-dashes t)
 (setq fci-dash-pattern .3)
 (setq fci-always-use-textual-rule nil)
+
 ;; ido
 (setq ido-enable-flex-matching t)
 (setq ido-auto-merge-inhibit-characters-regexp ".") ; don't change directories
 (setq ido-create-new-buffer 'always)
 (setq ido-use-faces t)
+(setq ido-use-virtual-buffers t)
+
 ;; indent guide mode
 (setq indent-guide-recursive nil)
+
 ;; js2-mode
 (setq js2-basic-offset 2)
 (setq js2-cleanup-whitespace t)
+
 ;; js3-mode
 (setq js3-basic-offset 2)
 (setq js3-cleanup-whitespace t)
@@ -167,8 +176,10 @@
 (setq magit-diff-options '("-w"))
 (setq magit-status-buffer-switch-function 'switch-to-buffer) ; don't split the window
 (setq magit-auto-revert-mode nil)
+
 ;; ruby
 (setq ruby-insert-encoding-magic-comment nil)
+
 ;; Scrolling and mouse
 (setq redisplay-dont-pause t
       scroll-margin 15
@@ -177,6 +188,7 @@
       scroll-preserve-screen-position 1)
 (setq mouse-wheel-follow-mouse 't)
 (setq mouse-wheel-scroll-amount '(1 ((shift) . 1)))
+
 ;; save minibuffer history
 (setq savehist-file (concat user-emacs-directory "savehist"))
 (savehist-mode 1)
@@ -185,18 +197,23 @@
       '(kill-ring
         search-ring
         regexp-search-ring))
+
 ;; scss
 (setq scss-compile-at-save nil)
+
 ;; spell check
 (setq ispell-program-name "aspell" ; use aspell instead of ispell
       ispell-extra-args '("--sug-mode=ultra"))
+
 ;; tramp
 (setq tramp-default-method "ssh")
+
 ;; web-mode
 (setq web-mode-enable-current-element-highlight nil)
 (setq web-mode-ac-sources-alist
       '(("css" . (ac-source-css-property))
         ("html" . (ac-source-words-in-buffer ac-source-abbrev))))
+
 ;; yasnippet
 (setq yas-snippet-dirs (append yas-snippet-dirs
                                '("~/src/emacs-config/snippets")))
